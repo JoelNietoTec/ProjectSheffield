35218e40b699f65996a328e215a2ec02ef37dd17
Joel Nieto <jonieto@galindo.gala>
2017-05-12 17:58:30 -0500

    Student

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule } from 'angular-calendar'
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
@@ -21,6 +23,8 @@ import { StudentDashboardModule } from './dashboards/student/student.module';
     HomeComponent
   ],
   imports: [
+    BrowserAnimationsModule,
+    CalendarModule.forRoot(),
     NgbModule.forRoot(),
     BrowserModule,
     FormsModule,
--- src/app/dashboards/student/student.module.ts
+++ src/app/dashboards/student/student.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { StudentComponent } from './student.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule } from 'angular-calendar';
+
 import { HomeworkComponent } from './homework/homework.component';
 import { HomeworkListComponent } from './homework-list/homework-list.component';
 
@@ -11,6 +14,8 @@ import { HomeworkListComponent } from './homework-list/homework-list.component';
   imports: [
     CommonModule,
     NgbModule.forRoot(),
+    BrowserAnimationsModule, // angular 4.0+ only
+    CalendarModule.forRoot()
   ],
   exports: [
     StudentComponent
